0cb94a6864f0bdd3297c152d4f7feaf9b1d283b9
robiulxasan <robiul15-2516@diu.edu.bd>
2022-11-19 19:09:37 +0600

    amazon demo using react

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,21 +1,28 @@
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import "./Product.css";
 
 const Product = (props) => {
   const { img, name, seller, price, stock } = props.product;
-  return ( 
+  return (
     <div className="product">
       <div className="">
         <img src={img} alt="" />
       </div>
       <div className="product-info">
         <h4 className="product-name">{name}</h4>
-        <p><small>By: {seller}</small></p>
+        <p>
+          <small>By: {seller}</small>
+        </p>
         <p>${price}</p>
         <p>Only {stock} left in stock - Order Soon </p>
-        <button className="cart-btn" onClick={()=>props.handleAddProduct(props.Product)}><FontAwesomeIcon icon={faShoppingCart} /> add to cart</button>
+        <button
+          className="cart-btn"
+          onClick={() => props.handleAddProduct(props.product)}
+        >
+          <FontAwesomeIcon icon={faShoppingCart} /> add to cart
+        </button>
       </div>
     </div>
   );
